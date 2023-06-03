36fa47aba8709ad43d6f6ff6ea0b70f7229c8beb
David <example@gmail.com>
2023-06-03 17:28:03 +0300

    BreakpointDebugger

---
+++ BreakpointDebug.js
@@ -0,0 +1,20 @@
+function hello(){
+	console.log("Hellow world");
+	debugger;
+};
+hello();
+
+function h1(){
+	console.log("Say hi");
+};
+
+h1();
+
+const arr = [1, 59, 40, 22];
+sorted = arr.sort(compreNum);
+
+function compreNum(a, b){
+	return a - b;
+};
+
+console.log(sorted);    
